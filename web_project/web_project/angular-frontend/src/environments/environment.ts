682fb9f64e1856634d9dfc408744f98d2bf96019
Juan Pablo Camacho <jcamacha@example.com>
2026-06-25 18:48:05 -0600

    fix: app component template, navbar+routeroutlet, production API URL

--- web_project/web_project/angular-frontend/src/environments/environment.ts
+++ web_project/web_project/angular-frontend/src/environments/environment.ts
@@ -1,1 +1,1 @@
-export const environment = { production: false, apiUrl: 'http://localhost:8080/api' };
+export const environment = { production: true, apiUrl: 'https://ml-lab-backend-production-c2db.up.railway.app/api' };
